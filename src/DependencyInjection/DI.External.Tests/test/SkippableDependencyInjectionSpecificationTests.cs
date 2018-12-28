83e89c71d893c81ed8c06abb4fda5fa8dea25b80
Pavel Krymets <pavel@krymets.com>
2018-12-27 16:10:34 -0800

    fix tests

--- src/DependencyInjection/DI.External.Tests/test/SkippableDependencyInjectionSpecificationTests.cs
+++ src/DependencyInjection/DI.External.Tests/test/SkippableDependencyInjectionSpecificationTests.cs
@@ -6,13 +6,23 @@ namespace Microsoft.Extensions.DependencyInjection.Specification
 {
     public abstract class SkippableDependencyInjectionSpecificationTests: DependencyInjectionSpecificationTests
     {
-        public virtual string[] SkippedTests { get; }
+        public virtual string[] SkippedTests { get; } = { };
+
+        public string[] DefaultSkippedTests { get; } = {
+            "IServiceProvider_Resolves_IServiceActivatorFactory",
+            "IServiceActivatorFactory_Create_ReturnsNullForUnregisteredService",
+            "IServiceActivator_GetService_ReturnsNewInstanceForRegisteredService",
+            "IServiceActivator_GetService_ResolvesInstanceFromProvidedScope",
+            "IServiceActivator_GetService_ReturnsSameInstanceForSingletons",
+        };
 
         protected sealed override IServiceProvider CreateServiceProvider(IServiceCollection serviceCollection)
         {
             foreach (var stackFrame in new StackTrace(1).GetFrames().Take(2))
             {
-                if (SkippedTests.Contains(stackFrame.GetMethod().Name))
+                var name = stackFrame.GetMethod().Name;
+                if (DefaultSkippedTests.Contains(name) ||
+                    SkippedTests.Contains(name))
                 {
                     // We skip tests by returning MEDI service provider that we know passes the test
                     return serviceCollection.BuildServiceProvider();
